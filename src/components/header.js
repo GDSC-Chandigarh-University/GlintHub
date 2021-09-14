bf41e938d2dd1386d9785be9131c80782903e5e8
Mohit Sharma <vipersharma.88@gmail.com>
2021-09-14 19:29:10 +0530

    Init Webpack

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>React</h2>
+            </div>
+        );
+    }
+}
