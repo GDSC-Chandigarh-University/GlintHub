6d806d28a18daa97f368b14c25af8508b5708592
Mohit Sharma <vipersharma.88@gmail.com>
2021-09-14 21:12:29 +0530

    Front page GlintHub

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,20 @@ export default class Header extends React.Component {
     render() {
         return (
             <div>
-                <h2>React</h2>
+                <header>
+
+                </header>
+                <section id="top">
+                    <div class="info">
+                        <h2>GlintHub</h2>
+                        <p>Make your projects published for FREE</p>
+                        <button>Get Started</button>
+                    </div>
+                    <div class="img">
+                        <img src="./assets/svg/GlintHub.svg" alt="GlintHut" />
+                        <img id="downarrow" src="./assets/svg/Downarrow.svg" alt="Downarrow" />
+                    </div>
+                </section>
             </div>
         );
     }
