e52b426dfc5ba7c60ff9fb13b1db2abf3fc47eb9
Mohit Sharma <vipersharma.88@gmail.com>
2021-09-16 20:00:15 +0530

    Update

--- src/components/header.js
+++ src/components/header.js
@@ -18,6 +18,18 @@ export default class Header extends React.Component {
                         <img id="downarrow" src="./assets/svg/Downarrow.svg" alt="Downarrow" />
                     </div>
                 </section>
+                <section id="top">
+                    <div class="info">
+                        <h2>Four Steps Process</h2>
+                        <p>Four easy steps to publish your
+                            applications to the storefronts.</p>
+                        <a>Learn more</a>
+                    </div>
+                    <div class="img">
+                        <img src="./assets/svg/GlintHub.svg" alt="GlintHut" />
+                        <img id="downarrow" src="./assets/svg/Downarrow.svg" alt="Downarrow" />
+                    </div>
+                </section>
             </div>
         );
     }
