73ec471aed3d51cb2ec770f9df922e4be223b4a5
Mohit Sharma <vipersharma.88@gmail.com>
2021-10-12 15:28:48 +0530

    Glinthub Components

---
+++ src/components/glintHub-landing/glintHub-landing-events.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class GlintHubLandingSteps extends React.Component {
+    render() {
+        return (
+            <div id="glinthub-landing-steps">
+                Content
+            </div>
+        );
+    }
+}
--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import React from "react";
 import Dashboard from "../glintHub-dashboard/dashboard";
-import Landing from "../glintHub-landing/landing"
+import GlintHubLanding from "../glintHub-landing/glintHub-landing";
 import AddApp from "../glintHub-dashboard/add-app";
 import DraftedApp from "../glintHub-dashboard/drafted-app";
 import PublishedApp from "../glintHub-dashboard/published-app";
@@ -11,7 +11,7 @@ const Router = (props) => {
     console.log(props);
     return (
         <BrowserRouter>
-            <Route path="/" component={Landing} exact="true">
+            <Route path="/" component={GlintHubLanding} exact="true">
             </Route>
             <Route path="/glinthub" component={Dashboard}>
             </Route>
