4205e910cb537ac473de104eb1b63fe9c98e415b
Atul <atuljoshi94170@gmail.com>
2021-10-12 20:56:15 +0530

    components merged

--- src/components/glintHub-landing/glintHub-landing-hero.js
+++ src/components/glintHub-landing/glintHub-landing-hero.js
@@ -4,7 +4,18 @@ export default class GlintHubLandingHero extends React.Component {
   render() {
     return (
       <div id="glinthub-landing-hero">
-        Content
+        <div id="hero">
+          <img
+            class="hero_img"
+            src="./hero_img.png"
+            alt="Vector image of a hut"
+          />
+          <div class="hero_content">
+            <h1>Glinthub</h1>
+            <p>Make your projects<br></br>published for FREE</p>
+            <a class="btn" href="">Get Started</a>
+          </div>
+        </div>
       </div>
     );
   }
