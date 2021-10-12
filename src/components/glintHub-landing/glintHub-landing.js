0bad9d195a49b39032c36c137c943eb9f69d5f9e
Mohit Sharma <vipersharma.88@gmail.com>
2021-10-13 00:39:41 +0530

    Added components

--- src/components/glintHub-landing/glintHub-landing.js
+++ src/components/glintHub-landing/glintHub-landing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../header/header";
 import GlintHubLandingHero from "./glintHub-landing-hero";
-import GlintHubLandingSteps from "./glintHub-landing-events";
+import GlintHubLandingSteps from "./glintHub-landing-steps";
 import GlintHubLandingProjects from "./glintHub-landing-projects";
 import GlintHubLandingFooter from "./glintHub-landing-footer";
 
