e339978d605df1d7db7be18adb0f7746a2d204d6
xxEasterGrymm <aryabhattacharyya.study@gmail.com>
2021-10-13 01:10:53 +0530

    hero section linked to the dashboard

--- src/components/glintHub-landing/glintHub-landing-hero.js
+++ src/components/glintHub-landing/glintHub-landing-hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class GlintHubLandingHero extends React.Component {
   render() {
@@ -6,14 +7,14 @@ export default class GlintHubLandingHero extends React.Component {
       <div id="glinthub-landing-hero">
         <div id="hero">
           <img
-            class="hero_img"
+            className="hero_img"
             src="./hero_img.png"
             alt="Vector image of a hut"
           />
-          <div class="hero_content">
+          <div className="hero_content">
             <h1>Glinthub</h1>
             <p>Make your projects<br></br>published for FREE</p>
-            <a class="btn" href="">Get Started</a>
+            <Link className="btn" to="/glinthub">Get Started</Link>
           </div>
         </div>
       </div>
