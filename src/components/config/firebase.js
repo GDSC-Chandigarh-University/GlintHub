f221927efefb1a0e576afd3134854d736b6b3e82
Mohit Sharma <vipersharma.88@gmail.com>
2021-10-26 23:21:31 +0530

    Firebase methods init

--- src/components/config/firebase.js
+++ src/components/config/firebase.js
@@ -1,26 +1,44 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import {GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged, signOut} from "firebase/auth"
+import { useState, useEffect } from "react";
 
-const config = {
-  apiKey: "AIzaSyD6EZWTyyIoL-3zugPdRWwOYIEgtK_1_js",
-  authDomain: "glinthub-ansh.firebaseapp.com",
-  projectId: "glinthub-ansh",
-  storageBucket: "glinthub-ansh.appspot.com",
-  messagingSenderId: "701729041237",
-  appId: "1:701729041237:web:e33677ba8a3b7890941ddc"
+const firebaseConfig = {
+  apiKey: "AIzaSyC2s2ojO76wtSU8fo0QnHHN3wYk47pscKw",
+  authDomain: "gdsccu-decd0.firebaseapp.com",
+  projectId: "gdsccu-decd0",
+  storageBucket: "gdsccu-decd0.appspot.com",
+  messagingSenderId: "876586180057",
+  appId: "1:876586180057:web:791ef6f49e9eb476818dc8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth()
+
+export const firestore = getFirestore(app)
+
+export async function GoogleAuthLogin() {
+  const provider = new GoogleAuthProvider()
+  return await signInWithPopup(auth,provider)
+  .catch((error) => {
+    console.log(error)
+  });
 }
 
-const app = initializeApp(config)
-
-const Signupwithgoogle = function() {
-  signInWithPopup(getAuth(app), new GoogleAuthProvider(app))
-      .then((result) => {
-        const user = result.user;
-        console.log(user.email);
-      }).catch((error) => {
-        const errorMessage = error.message;
-        console.log(errorMessage)
-      });
+export async function GoogleAuthLogout() {
+  await signOut(auth).catch((error) => {
+    console.log(error)
+  });
 }
 
-export default Signupwithgoogle;
+export function AuthState() {
+  const [currentUser, setcurrentUser] = useState()
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user)=>{
+      setcurrentUser(user)
+    })
+    return unsub
+  }, [])
+  return currentUser;
+}
