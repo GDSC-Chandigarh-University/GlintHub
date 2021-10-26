d89c3fb618b6d25e981c2c88c9a1264060fd0dd8
Mohit Sharma <vipersharma.88@gmail.com>
2021-10-26 23:23:26 +0530

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,14 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env', "@babel/preset-react"]
+                    presets: ['@babel/preset-env', "@babel/preset-react"],
+                    "plugins": [
+                        ["@babel/plugin-transform-runtime",
+                          {
+                            "regenerator": true
+                          }
+                        ]
+                      ]
                 }
             }
         }, {
