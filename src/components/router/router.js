fa3a8af71dae30fbd253e876b00247a23423b9be
Mohit Sharma <vipersharma.88@gmail.com>
2021-10-26 23:22:37 +0530

    Router set acc. to auth

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -6,23 +6,23 @@ import AddApp from "../glintHub-dashboard/glintHub-dashboard-add-app";
 import DraftedApp from "../glintHub-dashboard/glintHub-dashboard-drafted-app";
 import PublishedApp from "../glintHub-dashboard/glintHub-dashboard-published-app";
 import Reviews from "../glintHub-dashboard/glintHub-dashboard-reviews";
+import { AuthState } from "../config/firebase";
 
-const Router = (props) => {
-    console.log(props);
+const Router = () => {
+    const currentUser = AuthState()
     return (
         <BrowserRouter>
-            <Route path="/" component={GlintHubLanding} exact="true">
+        {!currentUser ? <Route path="/glinthub" component={GlintHubLanding} exact="true">
+            </Route> : <div><Route path="/glinthub" component={Dashboard}>
             </Route>
-            <Route path="/glinthub" component={Dashboard}>
-            </Route>
-            <Route path="/add-app" component={AddApp}>
+            <Route path="/glintub/add-app" component={AddApp}>
             </Route>
             <Route path="/published-app" component={PublishedApp}>
             </Route>
             <Route path="/drafted-app" component={DraftedApp}>
             </Route>
             <Route path="/reviews" component={Reviews}>
-            </Route>
+            </Route> </div>}
         </BrowserRouter>
     );
 }
