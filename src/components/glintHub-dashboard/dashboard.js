015a105972c49795d2fd4f7feaf9c181cf2f6569
Mohit Sharma <vipersharma.88@gmail.com>
2021-11-08 23:28:44 +0530

    Update dashboard UX

--- src/components/glintHub-dashboard/dashboard.js
+++ src/components/glintHub-dashboard/dashboard.js
@@ -1,43 +1,53 @@
 import React from "react";
 import GlintHubSidebar from "./glintHub-dashboard-sidebar";
-import Header from "../header/header"
-import { Switch, Route, useRouteMatch } from "react-router-dom";
+import Header from "../header/header";
+import { Switch, Route, withRouter } from "react-router-dom";
 import GlintubAddApp from "./glintHub-dashboard-add-app";
 import GlintHubDashboard from "./glintHub-dashboard";
 import GlintHubPublishedApp from "./glintHub-dashboard-published-app";
 import GlintHubDraftedApp from "./glintHub-dashboard-drafted-app";
 import GlintHubReviews from "./glintHub-dashboard-reviews";
+import { connect } from "react-redux";
+import Spinner from "../spinner/Spinner";
 
-export default function Dashboard() {
-    let { path } = useRouteMatch();
-    console.log(path)
-    return (
-        <div>
-            <Header></Header>
-            <div id="root">
-                <GlintHubSidebar></GlintHubSidebar>
-                <div id="root-1">
-                    <div className="upper-div">
-                        <Switch>
-                            <Route exact path={path}>
-                                <GlintHubDashboard/>
-                            </Route>
-                            <Route path={`${path}/add-app`}>
-                                <GlintubAddApp/>
-                            </Route>
-                            <Route path={`${path}/published-app`}>
-                                <GlintHubPublishedApp/>
-                            </Route>
-                            <Route path={`${path}/drafted-app`}>
-                                <GlintHubDraftedApp/>
-                            </Route>
-                            <Route path={`${path}/reviews`}>
-                                <GlintHubReviews/>
-                            </Route>
-                        </Switch>
+class Dashboard extends React.Component {
+    render() {
+        let { path } = this.props.match
+        return (
+            <div>
+                <Header></Header>
+                <div id="root">
+                    <GlintHubSidebar></GlintHubSidebar>
+                    <div id="root-1">
+                        <div className="upper-div">
+                            <Switch>
+                                {<Route exact path={path}>
+                                    <GlintHubDashboard currentUser={this.props.currentUser}/>
+                                </Route>}
+                                <Route path={`${path}/add-app`}>
+                                    <GlintubAddApp currentUser={this.props.currentUser}/>
+                                </Route>
+                                <Route path={`${path}/published-app`}>
+                                    <GlintHubPublishedApp currentUser={this.props.currentUser}/>
+                                </Route>
+                                <Route path={`${path}/drafted-app`}>
+                                    <GlintHubDraftedApp currentUser={this.props.currentUser}/>
+                                </Route>
+                                <Route path={`${path}/reviews`}>
+                                    <GlintHubReviews currentUser={this.props.currentUser}/>
+                                </Route>
+                            </Switch>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default withRouter(connect((state) => {
+    return {
+      currentUser: state.user.currentUser,
+      apps: state.apps
+    }
+  })(Dashboard))
--- src/components/glintHub-dashboard/glintHub-dashboard.js
+++ src/components/glintHub-dashboard/glintHub-dashboard.js
@@ -1,49 +1,163 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-import Orbus from "../../assets/images/Orbus.png"
-import AddApp from "../../assets/images/plus.png"
+import Orbus from "../../assets/images/Orbus.png";
+import AddApp from "../../assets/images/plus.png";
+import { connect } from "react-redux";
+import { appsInit, setPublishedApp, setDraftedApp, setReviewApp, appsLoaded } from "../actions";
+import { collection, onSnapshot, query, orderBy } from "@firebase/firestore";
+import { Firestore } from "../../firebase";
+import Spinner from "../spinner/Spinner";
 
 class GlintHubDashboard extends React.Component {
+    state = {
+        user: this.props.currentUser,
+        totalProjects: null,
+        publishedApps: [],
+        draftedApps: [],
+        reviewApps: [],
+        firstLoad: true
+    }
+
+    componentDidMount() {
+        this.props.appsInit()
+        this.setState(() => {
+            return {
+                publishedApps: [],
+                draftedApps: [],
+                reviewApps: []
+            }
+        })
+        const Query = query(collection(Firestore, "Users", this.state.user.uid, "Projects"), orderBy("timestamp", "desc"))
+        onSnapshot(Query, (snapshot) => {
+            snapshot.docs.map((doc) => {
+                if (doc.data().inReview) {
+                    this.props.setReviewApp(doc.data())
+                    this.setState((prevState) => {
+                        return {
+                            reviewApps: [
+                                ...prevState.reviewApps,
+                                doc.data()
+                            ]
+                        }
+                    })
+                } else if (doc.data().isDrafted) {
+                    this.props.setDraftedApp(doc.data())
+                    this.setState((prevState) => {
+                        return {
+                            draftedApps: [
+                                ...prevState.draftedApps,
+                                doc.data()
+                            ]
+                        }
+                    })
+                } else if (doc.data().isPublished) {
+                    this.props.setPublishedApp(doc.data())
+                    this.setState((prevState) => {
+                        return {
+                            publishedApps: [
+                                ...prevState.publishedApps,
+                                doc.data()
+                            ]
+                        }
+                    })
+                }
+            })
+            this.setState(() => {
+                return {
+                    totalProjects: snapshot.docs.length
+                }
+            })
+        });
+    }
+
+    componentDidUpdate() {
+        let { publishedApps, draftedApps, reviewApps, totalProjects, firstLoad } = this.state
+        if(totalProjects == publishedApps.length + reviewApps.length + draftedApps.length && firstLoad) {
+            console.log('Pump')
+            this.setState(() => {
+                return {
+                    firstLoad: false
+                }
+            })
+            this.props.appsLoaded()
+        }
+    }
+
     render() {
         let { url } = this.props.match
-        console.log(url)
-        return (
-                <div id="glinthub-dashboard">
-                    <div className="headingDash glinthub-dashboard-bold glinthub-dashboard-h1" id="board">Dashboard</div>
-                    <div className="dash-container-1 glinthub-dashboard-bold">
-                        {/* upper boxes */}
-                        <div className="bx-1">
-                            <div className="up-bx">Published Apps</div>
-                            <div className="dash-num">0</div>
-                        </div>
-
-                        <div className="bx-1 bx-clr-2">
-                            <div className="up-bx">Drafted Apps</div>
-                            <div className="dash-num">0</div>
-                        </div>
-
-                        <div className="bx-1 bx-clr-3">
-                            <div className="up-bx">In-review Apps</div>
-                            <div className="dash-num">12</div>
-                        </div>
-
+        let { publishedApps, draftedApps, reviewApps, firstLoad } = this.state
+        return firstLoad ? <Spinner/> : (
+            <div id="glinthub-dashboard">
+                <div className="headingDash glinthub-dashboard-bold glinthub-dashboard-h1" id="board">Dashboard</div>
+                <div className="dash-container-1 glinthub-dashboard-bold">
+                    {/* upper boxes */}
+                    <div className="bx-1">
+                        <div className="up-bx">Published Apps</div>
+                        <div className="dash-num">{publishedApps.length}</div>
                     </div>
 
-                    <div className="dash-container-2">
-                        {/* <!-- Upper part --> */}
-                        <div className="dashUp-1">
-                            <span id="pr">Projects</span>
-                            <span id="vie">View All</span>
-                        </div>
+                    <div className="bx-1 bx-clr-2">
+                        <div className="up-bx">Drafted Apps</div>
+                        <div className="dash-num">{draftedApps.length}</div>
+                    </div>
 
-                        <div className="bx-2">
-                            <div className="img-icn"><img src={Orbus} alt="Error" /></div>
-                            <Link to={`${url}/add-app`} className="img-icn-plus" ><img src={AddApp} width="33px" height="31px" alt="Error" /></Link>
-                        </div>
+                    <div className="bx-1 bx-clr-3">
+                        <div className="up-bx">In-Review Apps</div>
+                        <div className="dash-num">{reviewApps.length}</div>
                     </div>
                 </div>
+
+                <div className="dash-container-2">
+                    {/* <!-- Upper part --> */}
+                    <div className="dashUp-1">
+                        <span id="pr">Published Apps</span>
+                        <span id="vie">View All</span>
+                    </div>
+
+                    <div className="bx-2">
+                        {publishedApps.map((app) => {
+                            return (
+                                <div key={app.id} className="img-icn"><img src={Orbus} alt="Error" /></div>
+                            )
+                        })}
+                        <Link to={`${url}/add-app`} className="img-icn-plus" ><img src={AddApp} width="33px" height="31px" alt="Error" /></Link>
+                    </div>
+                </div>
+                <div className="dash-container-2">
+                    {/* <!-- Upper part --> */}
+                    <div className="dashUp-1">
+                        <span id="pr">Drafted Apps</span>
+                        <span id="vie">View All</span>
+                    </div>
+
+                    <div className="bx-2">
+                        {draftedApps.map((app) => {
+                            return (
+                                <div key={app.id} className="img-icn"><img src={Orbus} alt="Error" /></div>
+                            )
+                        })}
+                        <Link to={`${url}/add-app`} className="img-icn-plus" ><img src={AddApp} width="33px" height="31px" alt="Error" /></Link>
+                    </div>
+                </div>
+                <div className="dash-container-2">
+                    {/* <!-- Upper part --> */}
+                    <div className="dashUp-1">
+                        <span id="pr">In-reveiw Apps</span>
+                        <span id="vie">View All</span>
+                    </div>
+
+                    <div className="bx-2">
+                        {reviewApps.map((app) => {
+                            return (
+                                <div key={app.id} className="img-icn"><img src={Orbus} alt="Error" /></div>
+                            )
+                        })}
+                        <Link to={`${url}/add-app`} className="img-icn-plus" ><img src={AddApp} width="33px" height="31px" alt="Error" /></Link>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
 
-export default  withRouter(GlintHubDashboard)
+export default withRouter(connect(null, { appsInit, setPublishedApp, setDraftedApp, setReviewApp, appsLoaded })(GlintHubDashboard))
