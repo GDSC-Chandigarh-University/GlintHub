57d18dca351fb897e0a305d390b723fcef79a34a
Mohit Sharma <vipersharma.88@gmail.com>
2021-11-15 23:15:55 +0530

    Optimised modal.js

--- src/components/glintHub-dashboard/Modal.js
+++ src/components/glintHub-dashboard/Modal.js
@@ -1,28 +1,118 @@
 import React from "react";
+import ReactModal from "react-modal";
+import {
+    collection,
+    onSnapshot,
+    query,
+    orderBy,
+    doc,
+    updateDoc,
+    deleteDoc,
+} from "@firebase/firestore";
+import { connect } from "react-redux";
+import { updateApp, deleteApp } from "../actions";
+import { Firestore } from "../../firebase";
 
-export default class Modal extends React.Component {
+class Modal extends React.Component {
     state = {
+        user: this.props.user,
+        title: this.props.modalApp.title,
+        technology: this.props.modalApp.technology,
+        githubURL: this.props.modalApp.githubURL,
+        imageURL: this.props.modalApp.imageURL,
+        description: this.props.modalApp.description,
         modalApp: this.props.modalApp,
-        modalIsOpen: this.props.modalIsOpen
-    }
-    closeModal = () => {
-        this.props.closeModal()
-    }
+    };
+
+    handleChange = (event) => {
+        this.setState(() => {
+            return {
+                [event.target.name]: event.target.value,
+            };
+        });
+    };
+    handleUpdateModal = async () => {
+        let {
+            user,
+            modalApp,
+            title,
+            technology,
+            description,
+            githubURL,
+            imageURL,
+        } = this.state;
+        const data = {
+            title,
+            technology,
+            imageURL,
+            description,
+            githubURL
+        }
+        await updateDoc(
+            doc(Firestore, "Users", user.uid, "Projects", modalApp.id), data
+        );
+        this.props.updateApp(modalApp, data)
+        this.props.closeModal();
+    };
+
+    handleDeleteModal = async () => {
+        let { user, modalApp } = this.state;
+        await deleteDoc(doc(Firestore, "Users", user.uid, "Projects", modalApp.id));
+        this.props.deleteApp(modalApp)
+        this.props.closeModal();
+    };
+
     render() {
-        let { modalApp, modalIsOpen } = this.state
+        let { title, description, githubURL, imageURL, technology } = this.state;
         return (
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={this.closeModal}
+            <ReactModal
+                isOpen={this.props.modalIsOpen}
+                onRequestClose={this.props.closeModal}
                 ariaHideApp={false}
             >
-                <button onClick={this.closeModal}>close</button>
-                <div>Add new Channel</div>
-                {/* <input type="text" name="channelName" value={this.state.channelName} onChange={this.handleChange} />
-                <input type="text" name="channelDetails" value={this.state.channelDetails} onChange={this.handleChange} />
-                <button onClick={this.handleSubmit}>Submit</button>
-                <p>{errors}</p> */}
-            </Modal>
-        )
+                <div>
+                    <div>Update App</div>
+                    <div>
+                        <input
+                            type="text"
+                            name="title"
+                            value={title}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="technology"
+                            value={technology}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="description"
+                            value={description}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="imageURL"
+                            value={imageURL}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="githubURL"
+                            value={githubURL}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <button onClick={this.handleUpdateModal}>Update App</button>
+                        <button onClick={this.handleDeleteModal}>Delete App</button>
+                        <button onClick={this.props.closeModal}>Close</button>
+                    </div>
+                </div>
+            </ReactModal>
+        );
     }
 }
+
+export default connect(null , { updateApp, deleteApp })(Modal)
