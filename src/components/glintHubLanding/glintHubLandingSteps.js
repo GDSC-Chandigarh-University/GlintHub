9bf950cdcd30b34016086659f01f6199bac4e884
Harshit Shukla <harshitshukla258@gmail.com>
2022-01-13 20:28:39 +0530

    steps card set - landing page

--- src/components/glintHubLanding/glintHubLandingSteps.js
+++ src/components/glintHubLanding/glintHubLandingSteps.js
@@ -9,9 +9,9 @@ import navigation from "../../assets/images/Icon_navigation.png"
 export default class GlintHubLandingSteps extends React.Component {
     render() {
         return (
-            <div id="glinthubLandingSteps" className="mt-100">
-                <div className="flex">
-                    <div className="boxFlex mr-4 mb-5">
+            <div id="glinthubLandingSteps" className="mt-100 glinthubLandingSteps_main_div">
+                <div className="flex glinthubLandingSteps_left_div">
+                    <div className="boxFlex mr-4 mb-5 glinthubLandingSteps_left_div1">
                         <h3 className="ml-2 mb-2 fw-600">Submit</h3>
                         <div className="box mb-4 bg-cl-p-2 p-4">
                             <h4 className="lh-28">Submit your app to glintHub by submitting the given form with proper information.</h4>
@@ -21,7 +21,7 @@ export default class GlintHubLandingSteps extends React.Component {
                             <h4 className="lh-28">After mentor approval we will provide you with free domain and hosting to host your app.</h4>
                         </div>
                     </div>
-                    <div className="boxFlex ml-4 mt-5">
+                    <div className="boxFlex ml-4 mt-5 glinthubLandingSteps_left_div2">
                     <h3 className="ml-2 mb-2 fw-600">Review</h3>
                         <div className="box mb-4 p-4 bg-cl-lightGreen5">
                             <h4 className="lh-28">Mentors will review the submitted app and help you to improve your app via real time chat.</h4>
@@ -32,7 +32,7 @@ export default class GlintHubLandingSteps extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div className="stepContent">
+                <div className="stepContent glinthubLandingSteps_right_div">
                     <h1 className="medium-h1">Four Step Process</h1>
                     <h3 className="mb-3 fw-600 pl-1">Four easy steps to publish your applications  <br/>  to the storefronts.</h3>
                     <h3 className="fw-700 cl-lightGreen4 fw-600 hover" onClick={googleAuthLogin}>Get Started</h3>
